c6a61f0839b229a2cbc278e4909dc4ede35c02af
Prabhat Ranjan <www.encodedprogrammer@gmail.com>
2022-05-23 19:30:44 +0530

    allowing image in user

--- stage/serializer.py
+++ stage/serializer.py
@@ -6,7 +6,7 @@ class JobWithPermission(ModelSerializer):
     
     class Meta:
         model = Jobs
-        exclude = ('unit_cost', 'isDelivered', 'isEmailing', 'total_cost', 'notes', 'overseer', 'design')
+        exclude = ('unit_cost', 'isDelivered', 'isEmailing', 'total_cost', 'notes', 'overseer')
 
 class MidOrderWithPermission(ModelSerializer):
     job = JobWithPermission()
