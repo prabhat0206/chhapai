aaf34060d98d26ff13553a41f636e2ce1f13ab70
Prabhat Ranjan <www.encodedprogrammer@gmail.com>
2022-05-31 13:14:09 +0530

    mid in job

--- stage/urls.py
+++ stage/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('user_process_jobs', UserViewByPermissions.as_view(), name='user_jobs'),
     path('user_upcoming_jobs', UpcomingJobsUser.as_view(), name='user_up'),
     path('edit_profile/<int:pk>', EditProfile.as_view(), name='edit_'),
-    path('job/<int:pk>', JobUpdateDestroyAPI.as_view(), name='stage_update'),
-    path('midorder/<int:mid>/<int:pk>', MidOrderUpdateDestroyAPI.as_view(), name='midorder'),
+    path('job/<int:mid>/<int:pk>',
+         JobUpdateDestroyAPI.as_view(), name='stage_update'),
+    path('midorder/<int:pk>', MidOrderUpdateDestroyAPI.as_view(), name='midorder'),
 ]
 
 
